7aefca3e64d2ddaf182810e1dc2daf37479d478a
BreakinBenny <fox.wernicke@gmail.com>
2025-08-27 00:38:43 +0200

    Fler lets som ska has i formuläret…

--- app/components/AddItemForm.tsx
+++ app/components/AddItemForm.tsx
@@ -1,18 +1,32 @@
-import { useId } from 'react';
+import React, { useId } from 'react';
 import { Alert, Button, SafeAreaView, Text, TextInput, View } from 'react-native';
 import { varor } from '../models/varor';
-//let uniqueId = 0;
 
 export default function AddItemForm() {
+    //let uniqueId = "";
     let category = 0;
+    let image = "";
+    let title = "";
+    let brand = "";
+    let size = "";
+    let pattern = "";
+    let store = 0;
+    let checkindate = "";
+    let sold: false;
+    let checkoutdate = "";
+    let price = 0;
     const uniqueId = useId();
-    //const handleSubmit = () => {}
+    const handleSubmit = () => {
+        
+    }
+    varor.map((vara) => console.log(vara.id, vara.title, vara.category));
 
     return (
         <SafeAreaView><View>
-            <Button title="Hur många varor?" onPress={() => console.log(varor.length, 'varor finns i listan.')} />
+            <Button title="Hur många varor?" onPress={() => console.log(`${varor.length} varor finns i listan.`)} />
             <Button title="Generera ID" onPress={(uniqueId) => console.log('Genererar ID för varan.')} />
-            <Text>1: Topp{'\n'}2: Byxor{'\n'}3: Skor{'\n'}4: Headwear{'\n'}5: Accessoarer{'\n'}6: Annat</Text>
+            
+            <Text style={{padding: 20}}>1: Topp{'\n'}2: Byxor{'\n'}3: Skor{'\n'}4: Headwear{'\n'}5: Accessoarer{'\n'}6: Annat</Text>
             <TextInput style={{ height: 100, width: 340, borderColor: 'gray', borderWidth: 1, marginBottom: 10, paddingLeft: 8 }}
                 placeholder="Vilken kategori har produkten (mellan 1 och 6)?"
                 placeholderTextColor={"red"}
@@ -30,6 +44,8 @@ export default function AddItemForm() {
                     console.log(`Du valde kategori ${category}.`);
                 }}
             />
+
+            <Button title="Lägg till vara" onPress={() => console.log('Lägger till varan i listan...')} />
         </View></SafeAreaView>
     );
 }

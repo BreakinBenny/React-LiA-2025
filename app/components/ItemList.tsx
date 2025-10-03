0a530f2f7fb3f9b6fb4ed6c2e703528254859df4
BreakinBenny <fox.wernicke@gmail.com>
2025-10-03 10:16:12 +0200

    editProduct ska till ItemList…

--- app/components/ItemList.tsx
+++ app/components/ItemList.tsx
@@ -13,23 +13,20 @@ const loadItems = async () => {
 }
 console.log(`${varor.length} varor finns i listan.`);
 
-export default function ItemList({ navigation, deleteProduct, products}:
-    { navigation: any, deleteProduct: (product: Product) => void,
-        editProduct: (product: Product) => void, products: Product[] }) {
+export default function ItemList({ navigation, deleteProduct, editProduct, products}: { navigation: any,
+    deleteProduct: (product: Product) => void, editProduct: (product: Product) => void, products: Product[] }) {
     return (
         <FlatList style={{margin: 30, backgroundColor: 'lightgray', maxHeight: 350, width: 400, padding: 10}}
         data={products} keyExtractor={item => item.id} renderItem={({ item }) => {
             return (
                 <View>
                     <Pressable key={item.id} style={{marginBottom: 10, padding: 10, backgroundColor: 'white', borderRadius: 5}}
-                    onPress={() => {
-                    navigation.navigate('Detailmeny', { item, products /*category: item.category, price: item.price,
+                    onPress={() => { navigation.navigate('Detailmeny', { item, products /*category: item.category, price: item.price,
                         checkindate: item.checkindate, checkoutdate: item.checkoutdate, sold: item.sold,
                         store: item.store, brand: item.brand, size: item.size, pattern: item.pattern,
                         color1: item.color1, color2: item.color2, title: item.title, pic: item.image,
-                        thumb: item.thumb*/
-                    })
-                    }}>
+                        thumb: item.thumb*/ })}}
+                    onPressIn={() => { console.log(`Redigerar vara...`); /*editProduct(item)*/ }}>
                         <View style={{flexDirection: 'row', gap: 5}}>
                             {!item.image ? <Image style={{width: 33, height: 33}} source={require('@/assets/images/react-logo.png')} /> : <Image source={{ uri: item.image }} />}
                             {!item.thumb ? <Image source={require('@/assets/images/tiny_logo.png')} /> : <Image source={{ uri: item.thumb }} />}
